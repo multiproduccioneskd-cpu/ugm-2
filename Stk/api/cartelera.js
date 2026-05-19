d8fb26ef0444ed4354910894d0fe2cb4abeb1178
multiproduccioneskd-cpu <multiproducciones.kd@gmail.com>
2026-05-19 17:59:27 -0400

    Update cartelera.js

--- Stk/api/cartelera.js
+++ Stk/api/cartelera.js
@@ -1,5 +1,3 @@
-const https = require('https');
-
 module.exports = async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -10,7 +8,7 @@ module.exports = async function handler(req, res) {
     try {
         const { TENANT_ID, CLIENT_ID, CLIENT_SECRET, LIST_ID } = process.env;
 
-        // 1. Obtener Token de Azure Identity de forma nativa
+        // 1. Obtener Token de Azure Identity
         const tokenUrl = `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/token`;
         const params = new URLSearchParams({
             client_id: CLIENT_ID,
@@ -32,13 +30,9 @@ module.exports = async function handler(req, res) {
         const tokenData = await tokenRes.json();
         const accessToken = tokenData.access_token;
 
-        // IDs limpios en bruto estructurados para que no los toque el compilador
-        const hostName = "ugmchile.sharepoint.com";
-        const siteCollectionId = "0aaa32cc-4ad1-4d99-9ee6-78ede7cfff66";
-        const subSiteId = "a582523d-6b22-417e-bf2b-5b8b6e2fe62f";
-
-        // URL armada sin variables compuestas externas
-        const graphUrl = `https://graph.microsoft.com/v1.0/sites/${hostName},${siteCollectionId},${subSiteId}/lists/${LIST_ID}/items?expand=fields&$top=100`;
+        // 2. LA RUTA DEFINITIVA: Buscamos por el hostname y el path exacto de SharePoint
+        // Esto le dice a Microsoft que resuelva la ruta internamente, eliminando el itemNotFound
+        const graphUrl = `https://graph.microsoft.com/v1.0/sites/ugmchile.sharepoint.com:/sites/cartelera:/lists/${LIST_ID}/items?expand=fields&$top=100`;
 
         const graphRes = await fetch(graphUrl, {
             method: 'GET',
